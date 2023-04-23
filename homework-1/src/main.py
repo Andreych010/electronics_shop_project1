3139d0d939e2092069bb24769c486b9ffdb5a664
Андрей Марчук <marchuk-8282@mail.ru>
2023-04-23 14:26:55 +0200

    add src/main.py

---
+++ homework-1/src/main.py
@@ -0,0 +1,17 @@
+from item import Item
+
+if __name__ == '__main__':
+    item1 = Item("Смартфон", 10000, 20)  # экземпляры класса Item
+    item2 = Item("Ноутбук", 20000, 5)
+
+    print(f'Общая стоимость {item1.produkt} в магазинах: {item1.calculate_total_price()}')  # Обая стоимость товаров 200000
+    print(f'Общая стоимость {item2.produkt} в магазинах: {item2.calculate_total_price()}')  # 100000
+
+    # устанавливаем новый уровень цен
+    Item.pay_rate = 0.8
+    # применяем скидку
+    item1.apply_discount()
+
+    print(f'Стоимость {item1.produkt} со скидкой в {Item.pay_rate}: {item1.price} ')  # Стоимость товара со скидкой 8000.0
+    print(f'Стоимость {item2.produkt} ,без скидки: {item2.price}')  # Стоимость товара без скидки 20000
+    print(f'Общее наличие Товаров на складе: {Item.all}')  # Сохданные экземпляры класса Item
