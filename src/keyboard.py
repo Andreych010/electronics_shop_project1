3ec99983ca08f564bb575740eabe54a7cd638593
Андрей Марчук <marchuk-8282@mail.ru>
2023-05-06 21:32:52 +0200

    add keyboard.py

---
+++ src/keyboard.py
@@ -0,0 +1,48 @@
+from src.item import Item
+
+
+class MixinKey(Item):
+    '''
+    Реалезует хранение и изменение раскладки клавиатуры
+    '''
+
+    def __init__(self, name: str, price: float, quantity: int, language='EN'):
+        super().__init__(name, price, quantity)
+        self.__language = language
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__language})"
+
+    def __str__(self):
+        return f"{self.__language}"
+
+    @property
+    def language(self):
+        return self.__language
+
+    # @language.setter
+    # def language(self,new_language):
+    #     self.__language = new_language
+
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language = 'RU'
+            return self.__language
+        elif self.__language == 'RU':
+            self.__language = 'EN'
+            return self
+
+
+class KeyBoard(MixinKey, Item):
+    '''
+    Класс KeyBoard для товара “клавиатура”
+    '''
+
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f"{self.name}"
